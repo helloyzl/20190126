d5822805cffe35ee73e186ad2b60d4fd19a6c7bd
Dreamtobe <44310278+helloyzl@users.noreply.github.com>
2019-01-26 20:24:25 +0800

    Update test_report.py

--- scripts/test_report.py
+++ scripts/test_report.py
@@ -1,11 +1,11 @@
 import pytest,allure
 class Test_01:
-    @allure.step(title=‘这是测试1’)
+    @allure.step(title='这是第一个测试方法')
     def test_01(self):
         print('--test_01--')
         assert True
 
-    @allure.step(title=’这是测试2’)             
+    @allure.step(title='这是第二个测试方法')
     def test_02(self):
         print('--test_02---')
         assert False
