e78980402b3513740a18d3ab87f13ed819f06650
Dreamtobe <44310278+helloyzl@users.noreply.github.com>
2019-01-26 19:02:01 +0800

    Update test_report.py

--- scripts/test_report.py
+++ scripts/test_report.py
@@ -1,8 +1,11 @@
+import pytest， allure
 class Test_01:
+    @allure.step(title=‘这是测试1’)
     def test_01(self):
         print('--test_01--')
         assert True
 
+    @allure.step(title=’这是测试2’)             
     def test_02(self):
         print('--test_02---')
         assert False
